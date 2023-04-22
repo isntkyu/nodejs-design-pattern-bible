de39951f9e1f986a3c256dfd86bb093c448a82f4
이준규 <jkjkjk5710@gmail.com>
2023-04-22 19:34:58 +0900

    11장 비동기 작업 취소

---
+++ Chapter11-advanced-recipes/05-canceling-async/generator/createAsyncCancelable.js
@@ -0,0 +1,37 @@
+import { CancelError } from "../cancelError";
+
+export function createAsyncCancelable(generatorFunction) {
+  return function asyncCancelable(...args) {
+    const generatorObject = generatorFunction(...args);
+    let cancelRequested = false;
+
+    function cancel() {
+      cancelRequested = true;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+      async function nextStep(prevResult) {
+        if (cancelRequested) {
+          return reject(new CancelError());
+        }
+
+        if (prevResult.done) {
+          return resolve(prevResult.value);
+        }
+
+        try {
+          nextStep(generatorObject.next(await prevResult.value));
+        } catch (err) {
+          try {
+            nextStep(generatorObject.throw(err));
+          } catch (err2) {
+            reject(err2);
+          }
+        }
+      }
+      nextStep({});
+    });
+
+    return { promise, cancel };
+  };
+}
