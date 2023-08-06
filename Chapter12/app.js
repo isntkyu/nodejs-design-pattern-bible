85b756d31d32547109706259ad8b004d478c6737
isntkyu <jkjkjk5710@gmail.com>
2023-08-07 01:23:04 +0900

    에러, 장애, 재시작 시 가용성 유지 방법

--- Chapter12/app.js
+++ Chapter12/app.js
@@ -1,13 +1,39 @@
 import { createServer } from 'http'
 import { cpus } from 'os'
 import cluster from 'cluster'
+import { once } from 'events'
 
 
 if (cluster.isMaster) {
     const availableCpus = cpus()
     console.log(`Clustering to ${availableCpus.length} processes`)
     availableCpus.forEach(() => cluster.fork()) // 이 땐 isWorker가 true로 실행됨
+
+    cluster.on('exit', (worker, code) => {
+        if (code !== 0 && !worker.exitedAfterDisconnect) {
+            console.log(
+                `Worker ${worker.process.pid} crashed, ` +
+                `Starting a new worker`
+            )
+            cluster.fork()
+        }
+    })
+
+    process.on('SIGUSR2', async () => {
+        const workers = Object.values(cluster.workers)
+        for (const worker of workers) {
+            console.log(`Stopped worker: ${worker.process.pid}`)
+            worker.disconnect()
+            await once(worker, 'exit')
+            if (!worker.exitedAfterDisconnect) continue
+            const newWorker = cluster.fork()
+            await once(newWorker, 'listening')
+        }
+    })
 } else {
+    setTimeout(() => { throw new Error('Ooops') },
+        Math.ceil(Math.random() * 3) * 1000)
+
     const { pid } = process
     const server = createServer((req, res) => {
         let i = 1e7
